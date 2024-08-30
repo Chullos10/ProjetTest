aa3f311576508c76e292c43cb72af5623457d5e6
Karlos Ayala <karlosayalachozo@gmail.com>
2024-08-30 11:45:47 -0400

    Initial Commit

---
+++ ProjetTest/ProjetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetTestApp.swift
+//  ProjetTest
+//
+//  Created by Karlos Stevan Ayala Chozo (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct ProjetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
